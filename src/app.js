51756b5beea2567fa0c5753245c57c91293a2866
ihaabSaeed <ihaabsaid@gmail.com>
2020-07-05 19:23:19 +0200

    l

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+
+
+const app = express()
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname , '../Public')
+app.use(express.static(publicDirectoryPath))
+// app.get("/PlayGames",function(req,res){
+// 	res.sendFile(__dirname+"/Play/PlayGames.html")
+// })
+// app.get("/Contact",function(req,res){
+// 	res.sendFile(__dirname+"/Play/footer/test.html")
+// })
+// app.get("/About",function(req,res){
+// 	res.sendFile(__dirname+"/Play/footer/test.html")
+// })
+app.listen(port,()=>{
+	console.log('Server on 3000 avialible' + port)
+})
